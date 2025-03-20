b71cffec050e772befe255a666186278831e124a
Bianca Nechita <nechita,bianca@yahoo.com>
2025-03-20 12:46:15 -0600

    changes

--- components/Todo.js
+++ components/Todo.js
@@ -4,9 +4,26 @@ class Todo {
       this._templateElement = document.querySelector(selector);
     }
   
-    _getDueDateEl() {
-      this._dueDate = new Date(this._data.date);
+    _setEventListeners() {
+      this._todoCheckboxEl.addEventListener("change", () => {
+        this._data.completed = !this._data.completed;
+      });
   
+      this._todoDeleteBtn.addEventListener("click", () => {
+        this._todoElement.remove();
+      });
+    }
+  
+    _generateCheckboxEl() {
+      this._todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
+      this._todoLabel = this._todoElement.querySelector(".todo__label");
+      this._todoCheckboxEl.checked = this._data.completed;
+      this._todoCheckboxEl.id = `todo-${this._data.id}`;
+      this._todoLabel.setAttribute("for", `todo-${this._data.id}`);
+    }
+  
+    _generateDueDateEl() {
+      this._dueDate = new Date(this._data.date);
       if (!isNaN(this._dueDate)) {
         this._todoDate.textContent = `Due: ${this._dueDate.toLocaleString(
           "en-US",
@@ -19,40 +36,21 @@ class Todo {
       }
     }
   
-    _setEventListeners() {
-      this._todoDeleteBtn.addEventListener("click", () => {
-        this._todoDeleteBtn.remove();
-      });
-  
-      this._todoCheckboxEl.addEventListener("change", () => {
-        this._data.complated = !this._completed;
-      });
-    }
-  
-    _generateCheckboxEl() {
-      this._todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
-      this._todoLabel = this._todoElement.querySelector(".todo__label");
-      this._todoCheckboxEl.checked = this._data.completed;
-      this._todoCheckboxEl.id = `todo-${this._data.id}`;
-      this._todoLabel.setAttribute("for", `todo-${this._data.id}`);
-    }
-  
-    getView(data) {
+    getView() {
       this._todoElement = this._templateElement.content
         .querySelector(".todo")
         .cloneNode(true);
-  
-      this._todoNameEl = this._todoElement.querySelector(".todo__name");
+      const todoNameEl = this._todoElement.querySelector(".todo__name");
       this._todoDate = this._todoElement.querySelector(".todo__date");
       this._todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
-  
-      this._todoNameEl.textContent = this._data.name;
+      todoNameEl.textContent = this._data.name;
   
       this._generateCheckboxEl();
       this._setEventListeners();
-      this._getDueDateEl();
+      this._generateDueDateEl();
   
       return this._todoElement;
     }
   }
+  
   export default Todo;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import { v4 as uuidv4 } from "https://jspm.dev/uuid";
-
 import { initialTodos, validationConfig } from "../utils/constants.js";
 import Todo from "../components/Todo.js";
 import FormValidator from "../components/FormValidator.js";
@@ -18,7 +17,6 @@ const closeModal = (modal) => {
   modal.classList.remove("popup_visible");
 };
 
-// The logic in this function should all be handled in the Todo class.
 const generateTodo = (data) => {
   const todo = new Todo(data, "#todo-template");
   const todoElement = todo.getView();
@@ -27,6 +25,7 @@ const generateTodo = (data) => {
 
 addTodoButton.addEventListener("click", () => {
   openModal(addTodoPopup);
+  newTodoValidator.resetValidation();
 });
 
 addTodoCloseBtn.addEventListener("click", () => {
@@ -38,7 +37,6 @@ addTodoForm.addEventListener("submit", (evt) => {
   const name = evt.target.name.value;
   const dateInput = evt.target.date.value;
 
-  // Create a date object and adjust for timezone
   const date = new Date(dateInput);
   date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
 
@@ -46,8 +44,8 @@ addTodoForm.addEventListener("submit", (evt) => {
   const values = { name, date, id };
   const todo = generateTodo(values);
   todosList.append(todo);
+  newTodoValidator.resetValidation();
   closeModal(addTodoPopup);
-  evt.target.reset();
 });
 
 initialTodos.forEach((item) => {
@@ -56,5 +54,4 @@ initialTodos.forEach((item) => {
 });
 
 const newTodoValidator = new FormValidator(validationConfig, addTodoForm);
-
 newTodoValidator.enableValidation();
